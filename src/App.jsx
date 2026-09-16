9ed0e8f41af9fe86d121a42d5a190e798ccf12f4
Robert Hughes <rjh2869@gmail.com>
2026-09-16 12:23:44 -0500

    connected plant list to cart in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+
+import PLANTS from "./data";
+import Plants from "./plants/Plants";
+
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (plant) => {
+    const itemExists = cart.find((item) => {
+      return item.id === plant.id;
+    });
+
+    if (itemExists) {
+      const newCart = cart.map((cartItem) => {
+        return cartItem.id === plant.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem;
+      });
+
+      setCart(newCart);
+    } else {
+      const newItem = { ...plant, quantity: 1 };
+      setCart([...cart, newItem]);
+    }
+  };
+
+  const removeFromCart = (plant) => {
+    const newCart = cart.map((cartItem) => {
+      return cartItem.id === plant.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem;
+    });
+
+    const filteredCart = newCart.filter((cartItem) => {
+      return cartItem.quantity > 0;
+    });
+    setCart(filteredCart);
+  };
+  return (
+    <>
+      <h1>Welcome to Proper Plants</h1>
+
+      <main>
+        <Plants plants={PLANTS} addToCart={addToCart} />
+      </main>
+    </>
+  );
 }
