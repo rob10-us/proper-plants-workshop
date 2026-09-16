99881027be6fcb65e360920874a2f01f31c481d9
Robert Hughes <rjh2869@gmail.com>
2026-09-16 13:37:49 -0500

    maintained naming conventions with cartItem

--- src/cart/CartItem.jsx
+++ src/cart/CartItem.jsx
@@ -1,14 +1,14 @@
-export default function CartItem({ item, removeFromCart, addToCart }) {
+export default function CartItem({ cartItem, removeFromCart, addToCart }) {
   return (
     <li className="cart-item">
       <div>
-        {item.image} {item.name}
+        {cartItem.image} {cartItem.name}
       </div>
 
       <div className="cart-item-quantity">
-        <button onClick={() => removeFromCart(item)}>-</button>
-        {item.quantity}
-        <button onClick={() => addToCart(item)}>+</button>
+        <button onClick={() => removeFromCart(cartItem)}>-</button>
+        {cartItem.quantity}
+        <button onClick={() => addToCart(cartItem)}>+</button>
       </div>
     </li>
   );
